e1970543d56c2a1215f4670dc0611dcde08e5176
raffzsx33 <rafa.577santos@gmail.com>
2025-04-15 21:34:04 +0000

    SPtrunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -10,10 +10,12 @@ int main() {
     char Estado1 [20],Estado2 [20];
     char CodCarta1[20],CodCarta2[20]; // Código da carta
     char Cidade1[20],Cidade2 [20];
-    int População1,População2;
+    float População1,População2;
     float Área1,Área2;
     float PIB1,PIB2;
     int NPturísticos1,NPturísticos2; // Número de pontos turísticos
+    float DP1,DP2;   //Densidade Populacional
+    float PPC1,PPC2; //PIB per capita 
 
 
     printf ("Digite Estado da sua cidade: \n");
@@ -26,7 +28,7 @@ int main() {
     scanf ("%s", &Cidade1);
 
     printf ("Digite o número da População: \n");
-    scanf ("%d", &População1);
+    scanf ("%f", &População1);
 
     printf ("Digite o número da Área: \n");
     scanf ("%f", &Área1);
@@ -54,7 +56,7 @@ int main() {
     scanf ("%s", &Cidade2);
 
     printf ("Digite o número da População: \n");
-    scanf ("%d", &População2);
+    scanf ("%f", &População2);
 
     printf ("Digite o número da Área: \n");
     scanf ("%f", &Área2);
@@ -67,47 +69,37 @@ int main() {
     scanf ("%d", &NPturísticos2);
 
 
-    printf ("CARTA1: \n"); 
 
-    
     printf ("Estado: %s\n",Estado1);
     printf ("Código da carta: %s\n",CodCarta1);
     printf ("Nome da Cidade: %s\n",Cidade1);
-printf ("População:  %d\n", População1);
+printf ("População:  %f\n", População1);
 printf ("Área:  %f\n", Área1);
 printf ("PIB:  %f\n", PIB1);
 printf ("Número de pontos turísticos:  %d\n", NPturísticos1);
 
-    printf ("CARTA2: \n");
 
-
- printf ("Estado: %s\n",Estado2);
+ printf ("Estado: %s\n",Estado2); 
     printf ("Código da carta: %s\n",CodCarta2);
 printf ("Nome da Cidade: %s\n",Cidade2);
-printf ("População:  %d\n", População2);
+printf ("População:  %f\n", População2);
 printf ("Área:  %f\n", Área2);
 printf ("PIB:  %f\n", PIB2);
 printf ("Número de pontos turísticos:  %d\n", NPturísticos2);
 
-    
-    if (População1 > População2 ) {
-        printf("A população da carta 1 é maior! Carta 1 vence.\n");
-      } else {
-        printf("A população da carta 2 é maior! Carta 2 vence.\n");
-      }
 
+    DP1 = População1 / Área1;
+   printf ("A Densidade Populacional é: %.2f \n", DP1);
+    
+    DP2 = População2 / Área2;
+   printf (" A Densidade Populacional é:  %.2f \n", DP2);
+
+   PPC1 = PIB1 / População1;
+   printf ("PIB per capita é: %.2f \n", PPC1);
+   
+   PPC2 = PIB2 / População2;
+   printf ("PIB per capita é: %.2f \n", PPC2);
 
     return 0;
-
-    /*
-    
-    %d: Imprime um inteiro no formato decimal.
-    %i: Equivalente a %d.
-    %f: Imprime um numero de ponto flutuante no formato padrão.
-    %e: Imprime um número de ponto flutuante na notação científica.
-    %c: Imprime um único caractere.
-    %s: Imprime uama cadeia (string) de caracteres.
-
-    */
 
 }
